5a656987a6e324519c8ebb371bb623e67a79b0a6
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-03 11:28:25 +0200

    create getAllUsers function to get all users from db

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,13 @@
-export const getAllUsers = () => {
-  //get all users
+import { Request, Response, NextFunction } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    //get all users
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "ERROR", cause: error.message });
+  }
 };
