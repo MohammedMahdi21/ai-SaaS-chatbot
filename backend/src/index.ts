397ff893e67d278126ae32984b214e22f426c9a9
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-03 10:18:11 +0200

    Update port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
 //connection and listeneres
+
+const PORT = process.env.PORT || 5000;
 connectToDatabase().then(() => {
-  app.listen(5000, () => console.log("Server Running & Connected To DB"));
+  app.listen(PORT, () => console.log("Server Running & Connected To DB"));
 }).catch((err) => console.log(err));
