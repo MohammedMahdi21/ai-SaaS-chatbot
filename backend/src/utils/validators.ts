8a3d8fdffef5653ee4975de452f53cdc24508708
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-04 11:53:04 +0200

    validate user signup input using express-validator

---
+++ backend/src/utils/validators.ts
@@ -0,0 +1,7 @@
+import { body } from "express-validator";
+
+const signupValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").trim().isEmail().withMessage("Email is required"),
+  body("password").trim().isLength({ min: 6 }).withMessage("Password is required")
+];
