654172263a110db04903781721d7358e9aa38e2e
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-05 10:27:14 +0200

    create final validate function middleware

--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -1,5 +1,24 @@
-import { body } from "express-validator";
+import { NextFunction, Request, Response } from "express";
+import { ValidationChain, body, validationResult } from "express-validator";
 
+//final validate function
+const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    for (let validation of validations) {
+      const result = await validation.run(req);
+      if (!result.isEmpty()) {
+        break;
+      }
+    }
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    res.status(422).json({ errors: errors.array() });
+  }
+}
+
+//signup validation rule function
 const signupValidator = [
   body("name").notEmpty().withMessage("Name is required"),
   body("email").trim().isEmail().withMessage("Email is required"),
