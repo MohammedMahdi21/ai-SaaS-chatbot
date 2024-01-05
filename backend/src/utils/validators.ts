372ce15232b5fe9a883002508cd09ebaab7f3ba9
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-05 09:54:57 +0200

    update

--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -3,5 +3,5 @@ import { body } from "express-validator";
 const signupValidator = [
   body("name").notEmpty().withMessage("Name is required"),
   body("email").trim().isEmail().withMessage("Email is required"),
-  body("password").trim().isLength({ min: 6 }).withMessage("Password is required")
+  body("password").trim().isLength({ min: 6 }).withMessage("Password should contain atleast 6 characters")
 ];
