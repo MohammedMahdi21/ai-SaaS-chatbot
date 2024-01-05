ace50b225f8519cbe41af51d8cc4aa368c9cb10c
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-05 10:39:19 +0200

    user signup validation

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { signupValidator, validate } from "../utils/validators.js";
 
 const userRouters = Router();
 
 userRouters.get("/", getAllUsers)
-userRouters.post("/signup", userSignup)
+userRouters.post("/signup",validate(signupValidator), userSignup);
 export default userRouters;
--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { ValidationChain, body, validationResult } from "express-validator";
 
 //final validate function
-const validate = (validations: ValidationChain[]) => {
+export const validate = (validations: ValidationChain[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     for (let validation of validations) {
       const result = await validation.run(req);
@@ -18,8 +18,8 @@ const validate = (validations: ValidationChain[]) => {
   }
 }
 
-//signup validation rule function
-const signupValidator = [
+//signup Validation chain array
+export const signupValidator = [
   body("name").notEmpty().withMessage("Name is required"),
   body("email").trim().isEmail().withMessage("Email is required"),
   body("password").trim().isLength({ min: 6 }).withMessage("Password should contain atleast 6 characters")
