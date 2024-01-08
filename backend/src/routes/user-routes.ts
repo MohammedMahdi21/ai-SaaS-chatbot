a3858abcd33583de15ca5861e01a4f70cb6ced0e
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-08 13:13:15 +0200

    debugging

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
 import { loginValidator, signupValidator, validate } from "../utils/validators.js";
 
 const userRouters = Router();
 
 userRouters.get("/", getAllUsers)
 userRouters.post("/signup",validate(signupValidator), userSignup);
-userRouters.post("/login",validate(loginValidator), userSignup);
+userRouters.post("/login",validate(loginValidator), userLogin);
 export default userRouters;
