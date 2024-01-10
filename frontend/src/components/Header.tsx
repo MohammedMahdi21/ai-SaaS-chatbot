6e8cea567167df488768eabc8518db30f320f564
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-01-10 12:50:34 +0200

    add logo component to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import { AppBar, Toolbar } from "@mui/material";
 import React from "react";
+import Logo from "./shared/Logo";
 
 const Header = () => {
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
-      <Toolbar sx={{ display: "flex" }}></Toolbar>
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+      </Toolbar>
     </AppBar>
   );
 };
