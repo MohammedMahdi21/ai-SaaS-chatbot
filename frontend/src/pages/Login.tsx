3eee0f8eb9045847887e07bc13ef7c58a606aaaf
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-05-04 16:23:27 -0400

    build the right side of the login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Login = () => {
   return (
@@ -7,7 +7,42 @@ const Login = () => {
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
         <img src="airobot.png" alt="Robot" style={{ width: "400" }} />
       </Box>
-      <Box></Box>
+      <Box
+        display={"flex"}
+        flex={{ sx: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        padding={2}
+        ml={"auto"}
+        mt={16}
+      >
+        <form
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              textAlign="center"
+              padding={2}
+              fontWeight={600}
+            >
+              Login
+            </Typography>
+          </Box>
+        </form>
+      </Box>
     </Box>
   );
 };
