103c56dbc6edc826dddc00a6281dcfe9b6dee17a
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2024-06-06 21:32:41 -0400

    Create onSubmit event listener

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,66 +1,76 @@
-import React from "react";
-import { Box, Button, Typography } from "@mui/material";
-import CustomizedInput from "../components/shared/CustomizedInput";
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import CustomizedInput from '../components/shared/CustomizedInput';
 import LoginIcon from '@mui/icons-material/Login';
 
 const Login = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log(email, password);
+  };
   return (
-    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
-      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="airobot.png" alt="Robot" style={{ width: "400" }} />
+    <Box width={'100%'} height={'100%'} display='flex' flex={1}>
+      <Box padding={8} mt={8} display={{ md: 'flex', sm: 'none', xs: 'none' }}>
+        <img src='airobot.png' alt='Robot' style={{ width: '400' }} />
       </Box>
       <Box
-        display={"flex"}
+        display={'flex'}
         flex={{ sx: 1, md: 0.5 }}
-        justifyContent={"center"}
-        alignItems={"center"}
+        justifyContent={'center'}
+        alignItems={'center'}
         padding={2}
-        ml={"auto"}
+        ml={'auto'}
         mt={16}
       >
         <form
+          onSubmit={handleSubmit}
           style={{
-            margin: "auto",
-            padding: "30px",
-            boxShadow: "10px 10px 20px #000",
-            borderRadius: "10px",
-            border: "none",
+            margin: 'auto',
+            padding: '30px',
+            boxShadow: '10px 10px 20px #000',
+            borderRadius: '10px',
+            border: 'none',
           }}
         >
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center"
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
             <Typography
-              variant="h4"
-              textAlign="center"
+              variant='h4'
+              textAlign='center'
               padding={2}
               fontWeight={600}
             >
               Login
             </Typography>
-            <CustomizedInput type="email" name="email" label="Email" />
-            <CustomizedInput type="password" name="password" label="Password" />
+            <CustomizedInput type='email' name='email' label='Email' />
+            <CustomizedInput type='password' name='password' label='Password' />
             <Button
-              type="submit"
+              type='submit'
               sx={{
                 px: 2,
                 py: 1,
                 mt: 2,
-                width: "400px",
+                width: '400px',
                 borderRadius: 2,
-                bgcolor: "#00fffc",
-                ":hover": {
-                  bgcolor: "white",
-                  color: "black",
+                bgcolor: '#00fffc',
+                ':hover': {
+                  bgcolor: 'white',
+                  color: 'black',
                 },
               }}
               endIcon={<LoginIcon />}
-            >Login</Button>
+            >
+              Login
+            </Button>
           </Box>
         </form>
       </Box>
